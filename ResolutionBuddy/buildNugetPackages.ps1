5fd14b260e5efe446ccd3304ce69d71dde074ac2
Dan Manning <dmanning23@hotmail.com>
2016-04-05 21:23:46 -0500

    ios nuget

--- ResolutionBuddy/buildNugetPackages.ps1
+++ ResolutionBuddy/buildNugetPackages.ps1
@@ -1,3 +1,3 @@
 nuget pack .\ResolutionBuddy.Android\ResolutionBuddy.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-
-nuget pack .\ResolutionBuddy.WindowsGL\ResolutionBuddy.WindowsGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\ResolutionBuddy.iOS\ResolutionBuddy.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\ResolutionBuddy.DesktopGL\ResolutionBuddy.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
