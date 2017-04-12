acad2c17434737cbfe317f8ccdb5f4964e917e24
Dan Manning <dmanning23@hotmail.com>
2017-04-12 17:56:25 -0500

    Fxied portriat mode bug

--- ResolutionBuddy/buildNugetPackages.ps1
+++ ResolutionBuddy/buildNugetPackages.ps1
@@ -1,2 +1,3 @@
+rm *.nupkg
 nuget pack .\ResolutionBuddy.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget push *.nupkg
+nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
